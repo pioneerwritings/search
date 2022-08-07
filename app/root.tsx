ed89feeaa820c5140f9086be86ac6ac43f3ff29b
Joel Rivera <pioneerwritings@gmail.com>
2022-08-07 14:09:51 -0400

    feat: add root error boundary

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from '@remix-run/node'
+import { ErrorBoundaryComponent } from '@remix-run/node'
 import {
   Links,
   LiveReload,
@@ -9,8 +10,9 @@ import {
 } from '@remix-run/react'
 
 import { Header, HeaderLinks } from './components'
-import styles from '../app/styles/global/global.css'
-import reset from '../app/styles/global/reset.css'
+import styles from './styles/global/app.css'
+import globals from './styles/global/global.css'
+import reset from './styles/global/reset.css'
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
@@ -21,7 +23,8 @@ export const meta: MetaFunction = () => ({
 export const links = () => [
   ...HeaderLinks(),
   { rel: 'stylesheet', href: reset },
-  { rel: 'stylesheet', href: styles }
+  { rel: 'stylesheet', href: globals },
+  { rel: 'stylesheet', href: styles },
 ]
 
 export default function App() {
@@ -33,11 +36,41 @@ export default function App() {
       </head>
       
       <body>
-        <Header />
-        <Outlet />
+        <div className='app'>
+          <Header />
+          <Outlet />
+        </div>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
+      </body>
+    </html>
+  )
+}
+
+export const ErrorBoundary: ErrorBoundaryComponent = ({ error }) => {
+  console.error(error.message)
+
+  return (
+    <html>
+      <head>
+        <title>Error</title>
+        <Meta />
+        <Links />
+      </head>
+
+      <body>
+        <div className='error-boundary'>
+          <span className='overlay'>
+            <img
+              src='/images/500.png'
+              alt='A picture of the 10 horned leopard beast described in Daniel chapter 7.'
+              tabIndex={0}
+            />
+          </span>
+          <p role='alert'>Oh no! Something went terribly wrong.</p>
+        </div>
+        <Scripts />
       </body>
     </html>
   )
