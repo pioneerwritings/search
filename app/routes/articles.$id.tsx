43c226b03a0f86e5f7b231a72e519857fa17ecd6
Joel Rivera <pioneerwritings@gmail.com>
2022-08-12 12:28:56 -0400

    feat: add series article next/prev

--- app/routes/articles.$id.tsx
+++ app/routes/articles.$id.tsx
@@ -9,9 +9,45 @@ export const loader: LoaderFunction = async ({ params }) => {
   const url = process.env.STRAPI_API_URL as string
   const res = await fetch(`${url}/articles/${id}`)
 
+  const article = await res.json()
+  const title: string = article.data.attributes.title
+  
+  if(!isSeries(title)){
+    return json({ article })
+  }
+
+  const name   = title.split('—').filter(str => !Number(str)).join('—').trim()
+  const res2   = await fetch(`${url}/series?filters[name][$eq]=${name.trim()}`)
+  const series = await res2.json()
+
+  if(!series.data.length){
+    return json({ article })
+  }
+  const num = series.data[0].attributes.articles.data.findIndex((a: any) => a.attributes.title === title) + 1
+  const len = series?.data[0].attributes.articles.data.length
+
+  const prevArticle = (): string | null => {
+    if(num > 1){
+      const index = num - 2
+      return series.data[0].attributes.articles.data[index].id
+    }
+    return null
+  }
+
+  const nextArticle = (): string | null => {
+    if(num < len){
+      return series.data[0].attributes.articles.data[num].id
+    }
+    return null
+  }
+
   return json({
-    article: await res.json(),
-    series: {}
+    article,
+    
+    series: {
+      prev: prevArticle(),
+      next: nextArticle()
+    }
   })
 }
 
@@ -23,7 +59,7 @@ export const links: LinksFunction = () => [
 ]
 
 export default function ArticlePage(){
-  const { article } = useLoaderData()
+  const { article, series } = useLoaderData()
   const { data: { attributes } } = article
   const { title, subtitle, author, body, periodical } = attributes
 
