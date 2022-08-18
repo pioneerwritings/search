e3de5c2f31f9c1d950d042f568e5999352f67134
Joel Rivera <pioneerwritings@gmail.com>
2022-08-18 17:15:49 -0400

    chore: add card styles

---
+++ app/styles/components/card.ts
@@ -0,0 +1,62 @@
+export const styles = {
+  card: `
+    transition-all
+    rounded-lg
+    border
+    border-solid
+    max-w-[368px]
+    h-full
+    px-4
+    pt-4
+    pb-6
+    cursor-pointer
+    text-left
+    bg-white
+    lg:hover:shadow-lg
+    lg:hover:shadow-cornflower/40
+  `,
+  header: `
+    flex
+    justify-start
+  `,
+  author: `
+    text-ash
+    mb-1
+  `,
+  series: `
+    text-indigo
+    font-bold
+    mb-1
+    ml-4
+    px-2
+    pb-[2px]
+    rounded-full
+    bg-cornflower/10
+  `,
+  h2: `
+    leading-tight
+    font-heldane-bold
+    text-xl
+    mt-2
+    md:w-80
+    antialiased
+  `,
+  p: `
+    mt-2
+    mb-3
+    font-inter
+    font-light
+    leading-tight
+    antialiased
+  `,
+  topic: `
+    border
+    rounded-full
+    px-[12px]
+    py-1
+    font-inter
+    font-bold
+    text-xs
+    border-[#C9C9C9]
+  `
+}
