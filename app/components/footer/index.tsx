29072e6e172b0b8aab42ea3deeaee1e5ce8bb347
Joel Rivera <pioneerwritings@gmail.com>
2022-09-10 22:48:23 -0500

    chore: tweak footer text positioning

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -3,10 +3,8 @@ export const Footer = () => {
   const currentYear = new Date().getFullYear()
 
   return (
-    <footer className='w-full h-32 flex items-center justify-center bg-gray-50 mt-12'>
-      <p>
-        <strong>© {currentYear}</strong> Pioneer Writings
-      </p>
+    <footer className='w-full h-32 flex items-center justify-start md:justify-center bg-gray-50 mt-12 pl-10 md:p-0'>
+      <p><strong>© {currentYear}</strong> Pioneer Writings</p>
     </footer>
   )
 }
