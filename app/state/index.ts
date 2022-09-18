34a7f7c31cef02a721519aca2b556ac9692fd686
Joel Rivera <pioneerwritings@gmail.com>
2022-09-18 14:21:04 -0500

    feat: add footer state

--- app/state/index.ts
+++ app/state/index.ts
@@ -6,11 +6,24 @@ interface SearchState {
   results: any[]
 }
 
+interface FooterState {
+  active: boolean
+  bottom: boolean
+}
+
 export const searchState = atom<SearchState>({
   key: 'search',
   default: {
     active: false,
     query: '',
     results: []
+  }
+})
+
+export const footerState = atom<FooterState>({
+  key: 'footerState',
+  default: {
+    active: true,
+    bottom: true
   }
 })
