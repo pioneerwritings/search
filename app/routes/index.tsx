9aa30601ecc595ba5459081b9415103f094bb5b4
Joel Rivera <pioneerwritings@gmail.com>
2022-09-22 09:17:42 -0500

    chore: simplify home page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,6 @@
 import { LoaderFunction } from '@remix-run/node'
 import { useLoaderData, useFetcher } from '@remix-run/react'
-import { Card, Show, Dropdown } from '~/components'
+import { Card, TopicsCarousel, Show } from '~/components'
 import { useRecoilState } from 'recoil'
 import { isSeries, normalizeArticle } from '~/utils'
 import { useScrollBottom } from '~/hooks'
@@ -11,16 +11,15 @@ import { footerState } from '~/state'
 
 import {
   type Article,
+  type CMSTopicResponse,
   type CMSArticleResponse,
   type CMSTopic,
-  type Series,
-  type DropdownItem
+  type Series
 } from '~/types'
 
 import Spinner from 'react-spinner-material'
 import qs from 'qs'
 
-type SearchCategory = 'articles' | 'series' | 'topics'
 const LIMIT = 15
 
 export const loader: LoaderFunction = async ({ request }) => {
@@ -34,9 +33,11 @@ export const loader: LoaderFunction = async ({ request }) => {
   })
 
   const res = await fetchData<CMSArticleResponse>('articles', q)
+  const res2 = await fetchData<CMSTopicResponse>('topics')
 
   return {
     total: res.meta?.pagination?.total,
+    topics: res2.data.map(topic => topic.attributes.name),
     articles: res.data.map(
       normalizeArticle
     )
@@ -50,51 +51,46 @@ type FetcherResponse = {
   results?: Article[]
 }
 
-export default function Index() {
-  const initialData = useLoaderData<{ articles: Article[], total: number }>()
-  const { data, load, state } = useFetcher<FetcherResponse>()
+type LoaderData = {
+  topics: string[]
+  articles: Article[],
+  total: number
+}
 
-  const [ category, setCategory ] = useState<SearchCategory>('articles')
+export default function Index() {
+  const initialData = useLoaderData<LoaderData>()
+  
   const [ articles, setArticles ] = useState<Article[]>(initialData.articles)
   const [ results, setResults ] = useState<Article[]>()
-  const [ series, setSeries ] = useState<Series[]>()
-  const [ topics, setTopics ] = useState<string[]>([])
-  const [ topic, setTopic ] = useState<DropdownItem>()
+  const [ topic, setTopic ] = useState<string>()
   const [ start, setStart ] = useState<number>(LIMIT)
-  const [_, setFooterState] = useRecoilState(footerState)
+  const [ _, setFooterState ] = useRecoilState(footerState)
 
+  const { data, load, state } = useFetcher<FetcherResponse>()
   const { bottom } = useScrollBottom()
+  const { topics } = initialData
 
   const canScroll = (articles.length !== initialData.total)
 
-  useEffect(() => load('/api/topics'), [])
+  const articleList: Article[] = (
+    topic && results ? results : articles
+  )
 
   useEffect(() => {
-    if(category !== 'articles'){
-      return setFooterState({
-        active: true, bottom
-      })
-    }
     setFooterState({
-      active: !canScroll,
-      bottom: (!canScroll && bottom) ? true : false
+      active: !canScroll || !!topic,
+      bottom: (!canScroll && bottom) || !!topic && bottom
     })
-  }, [articles, bottom, canScroll])
+  }, [articles, bottom, canScroll, topic])
 
   useEffect(() => {
     if(topic){
-      load(`/api/results?topic=${topic.value}`)
+      load(`/api/results?topic=${topic}`)
     }
   }, [topic])
 
-  useEffect(() => {
-    if(category === 'series' && !series){
-      load('/api/series')
-    }
-  }, [category])
-
   useEffect(() => {    
-    if(canScroll && category === 'articles' && bottom){
+    if(canScroll && bottom && !topic){
       const qs = new URLSearchParams([
         ['pagination[start]', String(start)],
         ['pagination[limit]', String(LIMIT)]
@@ -105,16 +101,8 @@ export default function Index() {
 
   useEffect(() => {
     if(data !== undefined){
-      if(data.topics){
-        setTopics(data.topics.map(({ attributes }) => {
-          return attributes.name
-        }))
-      }
       if(data.results){
         setResults(data.results)
-      }
-      if(data.series){
-        setSeries(data.series)
       }
       if(data.articles){
         setStart(start + LIMIT)
@@ -123,11 +111,7 @@ export default function Index() {
     }
   }, [data])
 
-  const handleTabSelect = (tab: SearchCategory) => {
-    setCategory(tab)
-  }
-
-  const handleTopicChange = (topic: DropdownItem) => {
+  const handleTopicChange = (topic: string) => {
     setTopic(topic)
     
     // if(isProduction){
@@ -137,64 +121,14 @@ export default function Index() {
     // }
   }
 
-  const articleList = (): Article[] | null => {
-    if(category === 'topics' && results){
-      return results
-    }
-    if(category === 'articles') {
-      return articles
-    }
-    return null
-  }
-
   return (
     <div className={styles.page}>
-      <div
-        className={styles.tabs}
-        role='tablist'
-        aria-label='Select a category'>
-          {
-            ['Articles', 'Series', 'Topics'].map((tab) => {
-              const c = tab.toLowerCase() as SearchCategory, id = `${c}-tab`
-
-              return (
-                <button
-                  key={id} className={styles.tab}
-                  role='tab'
-                  aria-labelledby={id}
-                  aria-selected={c === category}
-                  onClick={() => handleTabSelect(c)}
-                  tabIndex={0}>
-                  <span id={id}>{tab}</span>
-                </button>
-              )
-            })
-          }
-      </div>
-
-      <Show when={category === 'topics'}>
-        <Dropdown
-          className='mx-auto max-w-[368px] mb-8'
-          selected={topic}
-          onChange={handleTopicChange}
-          placeholder='Select topic'
-          ariaLabel='Topics'
-          list={topics.map((topic, i) => ({ id: i, value: topic }) )}
-        />
-      </Show>
-
-      <Show when={
-        state === 'loading' && category === 'series' ||
-        state === 'loading' && category === 'topics'
-      }>
-        <Spinner
-          visible
-          color='#7570FA'
-          stroke={2}
-          radius={30}
-          className='mx-auto my-6'
-        />
-      </Show>
+      <TopicsCarousel
+        data={topics}
+        title='Topics'
+        onClick={handleTopicChange}
+        activeItem={topic ?? ''}
+      />
 
       <div className={styles.gridContainer}
         role='feed'
@@ -205,48 +139,26 @@ export default function Index() {
           aria-label='Article feed'
           role='grid'>
 
-          <Show when={category === 'articles' || category == 'topics'}>
-            {
-              articleList()?.map((article) => {
-                const { id, title } = article
-                const series = isSeries(title)
+          {
+            articleList?.map((article) => {
+              const { id, title } = article
+              const series = isSeries(title)
 
-                return (
-                  <Card
-                    {...article}
-                    type='articles'
-                    heading={title}
-                    key={id}
-                    seriesLabel={series ? 'Series' : ''}
-                  />
-                )
-              })
-            }
-          </Show>
-
-          <Show when={category === 'series'}>
-            {
-              series?.map(series => {
-                const { id, articles, name, description, ...s } = series
-
-                return (
-                  <Card
-                    {...s}
-                    type='series'
-                    seriesLabel={`${articles.length} Parts`}
-                    id={id}
-                    key={id}
-                    heading={name}
-                    excerpt={description}
-                  />
-                )
-              })
-            }
-          </Show>
+              return (
+                <Card
+                  {...article}
+                  type='articles'
+                  heading={title}
+                  key={id}
+                  seriesLabel={series ? 'Series' : ''}
+                />
+              )
+            })
+          }
         </div>
       </div>
 
-      <Show when={state === 'loading' && category === 'articles'}>
+      <Show when={state === 'loading'}>
         <Spinner
           visible
           color='#7570FA'
