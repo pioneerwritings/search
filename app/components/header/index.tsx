cf27b21eea17332658761d919c0104c0e329bdf3
Joel Rivera <pioneerwritings@gmail.com>
2022-10-16 20:47:35 -0500

    feat: add desktop nav, hamburger

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,21 +1,70 @@
-import { Link } from '@remix-run/react'
+import { Link, useLocation } from '@remix-run/react'
 import { useRecoilState } from 'recoil'
-import { searchState as SearchState } from '~/state'
+import { searchState as SearchState, navState as NavState } from '~/state'
+import { styles } from '~/styles/components/header'
+
+import classNames from 'classnames'
 
 export const Header = () => {
-  const [, setSearchState] = useRecoilState(SearchState)
+  const [state, setSearchState] = useRecoilState(SearchState)
+  const [_, setNavState] = useRecoilState(NavState)
+
+  const { pathname } = useLocation()
 
   const handleSearchClick = () => {
-    setSearchState(s => ({ ...s, active: true }))
+    setSearchState(({ ...state, active: true }))
+  }
+
+  const handleHamburgerClick = () => {
+    setNavState(prev => {
+      return { active: !prev.active }
+    })
+  }
+
+  const activeClass = (path: string) => {
+    if(path === pathname) {
+      return styles.activeRoute
+    }
   }
 
   return (
-    <header className='flex justify-between px-8 pt-6'>
-      <Link to='/' className='logo'>
+    <header className={styles.header}>
+      <Link to='/' className={styles.logo}>
         <img className='w-24' src='/images/logo.svg' alt='Go to the home page' />
       </Link>
 
       <button
+        type='button'
+        role='button'
+        className={styles.hamburger}
+        aria-label='Open the navigation menu'
+        onClick={handleHamburgerClick}>
+        <img src='/images/hamburger.svg' aria-hidden />
+      </button>
+
+      <img
+        className={styles.logomark}
+        src='/images/logomark.svg'
+        alt='Go to the home page'
+      />
+
+      <nav className={styles.nav}>
+        {
+          ['Explore', 'Contact', 'Donate', 'Series'].map((r) => {
+            const route = r === 'Explore' ? '/' : `/${r.toLowerCase()}`
+
+            const classes = classNames(
+              'mr-6 text-[#939393] hover:text-black', activeClass(route)
+            )
+            return (
+              <Link to={route} className={classes} key={r}>{r}</Link>
+            )
+          })
+        }
+      </nav>
+
+      <button
+        className={styles.search}
         type='button'
         role='button'
         aria-label='Search'
