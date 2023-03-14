cd017414e2653347a11d124f7ee1e81395872c47
Joel Rivera <pioneerwritings@gmail.com>
2023-03-13 20:33:50 -0500

    feat: add series page

--- app/routes/series.tsx
+++ app/routes/series.tsx
@@ -1,8 +1,53 @@
+import { json, LoaderArgs } from "@remix-run/node"
+import { fetchData } from '~/fetchers'
+import { normalizeSeries } from '~/utils'
+import { CMSSeriesResponse } from '~/types'
+import { useLoaderData } from "@remix-run/react"
+import { Card } from "~/components"
+
+export const loader = async ({}: LoaderArgs) => {
+  const { data } = await fetchData<CMSSeriesResponse>('series')
+  
+  return json({ 
+    seriesList: data.map(normalizeSeries)
+  })
+}
 
 export default function SeriesPage(){
+  const { seriesList } = useLoaderData<typeof loader>()
+
   return (
-    <div>
-      <h1>Series</h1>
-    </div>
+    <main className='w-full min-h-screen pt-16 pb-36 px-8 antialiased'>
+      <h1 className='leading-tight text-center font-heldane-bold text-3xl md:text-[2.5rem] antialiased'>
+        Stay Focused With Series.
+      </h1>
+
+      <p className='max-w-lg text-center antialiased font-light mx-auto mt-3 mb-10'>
+        Series allows for a more productive reading experience by grouping all articles
+        that belong to a series together in one place.
+      </p>
+
+      <div className='m-auto md:max-w-3xl lg:max-w-3xl xl:max-w-6xl' role='feed' aria-label='All the series'>
+        <div className='grid gap-6 w-full h-full grid-cols-[repeat(auto-fill,_minmax(320px,_1fr))] justify-items-center'>
+          {
+            seriesList.map((series) => {
+              const { id, name, description, ...attrs } = series
+
+              return (
+                <Card
+                  {...attrs}
+                  id={id}
+                  excerpt={description}
+                  type='series'
+                  heading={name}
+                  key={id}
+                  seriesLabel='Series'
+                />
+              )
+            })
+          }
+        </div>
+      </div>
+    </main>
   )
 }
