836264c8cd85192473b0f5353f2087622dc1a29e
Joel Rivera <pioneerwritings@gmail.com>
2023-03-16 10:24:01 -0500

    fix: revert useAlgoliaClient to use window.env

--- app/hooks/useAlgoliaClient.ts
+++ app/hooks/useAlgoliaClient.ts
@@ -1,17 +1,16 @@
 import { useState, useEffect } from 'react'
-import { useEnv } from '~/hooks'
+import { CustomWindow } from '~/types'
 
 import search from 'algoliasearch/lite'
+declare const window: CustomWindow
 
 export const useAlgoliaClient = () => {
   const [client, setClient] = useState<any>()
 
   useEffect(() => {
-    const { ALGOLIA_APP_ID, ALGOLIA_SEARCH_KEY } = useEnv()
-    
     setClient(search(
-      ALGOLIA_APP_ID!,
-      ALGOLIA_SEARCH_KEY!
+      window.env.ALGOLIA_APP_ID,
+      window.env.ALGOLIA_SEARCH_KEY
     ))
   }, [])
 
