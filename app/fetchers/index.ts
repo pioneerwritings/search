086e1b14b5acb1817bd8216c803efa8778681d7b
Joel Rivera <pioneerwritings@gmail.com>
2023-03-16 10:25:16 -0500

    fix: revert fetchData env call

--- app/fetchers/index.ts
+++ app/fetchers/index.ts
@@ -1,9 +1,11 @@
-import { useEnv } from '~/hooks'
+import { CustomWindow } from '~/types'
+import { isServer } from '~/config'
+
+declare const window: CustomWindow
+const env = !isServer ? window.env : process.env
 
 export const fetchData = async <T>(type: string, query?: string) => {
-  const { STRAPI_API_URL } = useEnv()
-  
-  const url = `${STRAPI_API_URL}/${type}`
+  const url = `${env.STRAPI_API_URL}/${type}`
   const res = await fetch(query ? `${url}?${query}` : url)
 
   return await res.json() as T
