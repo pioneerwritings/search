a75cf7e3a1df77a9e7a821044cdde3f5ce2e54ac
Joel Rivera <pioneerwritings@gmail.com>
2023-03-16 10:16:24 -0500

    feat: update to read from ga4 global state

--- app/hooks/useGoogleAnalytics.ts
+++ app/hooks/useGoogleAnalytics.ts
@@ -1,36 +1,15 @@
-import GA4ReactResolveInterface, { GA4React } from 'ga-4-react'
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import { useLocation } from '@remix-run/react'
-import { useEnv } from '~/hooks'
+import { useRecoilValue } from 'recoil'
+import { GA4State } from '~/state'
 
-export const useGoogleAnalytics = (): { GA4: GA4ReactResolveInterface } => {
-  const [ GA4, setGA4 ] = useState<any>()
+export const useGoogleAnalytics = () => {
   const { pathname } = useLocation()
+  const GA4 = useRecoilValue(GA4State)
 
-  useEffect(() => {
-    const { GA_TRACKING_ID, NODE_ENV } = useEnv()
-    const id  = GA_TRACKING_ID!
-    const prod = NODE_ENV !== 'production'
-
-    const g4react = new GA4React(id, { 
-      debug_mode: !prod 
-    })
-
-    if(prod){
-      g4react.initialize()
-      .then((ga4: any) => setGA4(ga4))
-      .catch(err => console.error(err))
-    }
-  }, [])
-
-  useEffect(() => {
-    const { NODE_ENV } = useEnv()
-    const prod = NODE_ENV !== 'production'
-
-    if(GA4 && prod){
-      GA4?.pageview(pathname)
-    }
-  }, [ GA4, pathname ])
+  useEffect(() => GA4?.pageview(pathname), 
+    [ GA4, pathname ]
+  )
 
   return { GA4 }
 }
